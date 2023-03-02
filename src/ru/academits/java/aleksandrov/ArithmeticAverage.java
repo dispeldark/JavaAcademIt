af9b137d34b8665fd208afc67f463d5e814789d0
Dispeldark <revealder@yandex.ru>
2023-03-02 10:43:06 +0700

    Начальное и конечное значения диапазона теперь запрашиваются с консоли; решение переделано для одного цикла

--- src/ru/academits/java/aleksandrov/ArithmeticAverage.java
+++ src/ru/academits/java/aleksandrov/ArithmeticAverage.java
@@ -1,26 +1,30 @@
 package ru.academits.java.aleksandrov;
 
+import java.util.Scanner;
+
 public class ArithmeticAverage {
     public static void main(String[] args) {
-        int firstNumber = 3;
-        int lastNumber = 17;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Введите последнее число: ");
+        int lastNumber = scanner.nextInt();
 
         int numbersCount = 0;
         int numbersSum = 0;
-
-        for (int i = firstNumber; i <= lastNumber; i++) {
-            numbersSum += i;
-            numbersCount++;
-        }
-
         int evenNumbersCount = 0;
         int evenNumbersSum = 0;
 
-        for (int j = firstNumber; j <= lastNumber; j++) {
-            if (j % 2 == 0) {
-                evenNumbersSum += j;
+        for (int i = firstNumber; i <= lastNumber; i++) {
+            if (i % 2 == 0) {
+                evenNumbersSum += i;
                 evenNumbersCount++;
             }
+
+            numbersSum += i;
+            numbersCount++;
         }
 
         double numbersArithmeticAverage = (double) numbersSum / numbersCount;
